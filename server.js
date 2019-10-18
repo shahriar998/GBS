beab9ad1393bc3f3f206f4185cf2febd43c67c10
Shahriar Elias <shahriar998@gmail.com>
2019-10-18 18:47:05 -0400

    adds err parameter

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const connection = mongoose.connection;
 connection.on("connected", () => {
     console.log("Mongoose connected successfully");
 });
-connection.on("error", () => {
+connection.on("error", (err) => {
     console.log("Mongoose default connection error: " + err);
 });
 
