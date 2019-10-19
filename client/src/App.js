1b4307ac4e47ae0a51433131970c0ecc48f549a6
Shahriar Elias <shahriar998@gmail.com>
2019-10-18 21:38:06 -0400

    pages setup done

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
-import Home from './containers/Home';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
 
 function App() {
-  return <>
-    <Home />
-  </>;
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Books} />
+          <Route exact path="/results" component={Books} />
+          <Route exact path="/saved" component={Detail} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
